0d719564ea2a00e508d1117a562820d497da130a
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-15 04:40:12 -0700

    adding TASK:4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+
+/**
+ * main - Entry point of the program
+ * prints something into the console then exits with SUCCESS code
+ * Return: Always return 0 (SUCCESS)
+ */
+
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
 
