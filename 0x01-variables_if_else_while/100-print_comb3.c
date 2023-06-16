561710de34611341aea7a4c993286a9ef49158b9
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-16 23:20:49 +0200

    adding TASK:100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * function a program that prints all possible
+ * combinations of single-digit numbers.
+ * Return: 0 Always (SUCCESS)
+ * 00 01 02 03
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+			if (i == 8 && j == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
