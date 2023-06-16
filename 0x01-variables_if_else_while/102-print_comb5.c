65cf96b1b75cb40bcc598a68bf4032229297c024
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-17 00:02:24 +0200

    adding TASK:102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	int x, y;
+	int x = 0, y = 1;
 
 	while (x < 100)
 	{
@@ -21,6 +21,7 @@ int main(void)
 			putchar(' ');
 			putchar(y / 10 + '0');
 			putchar(y % 10 + '0');
+
 			if (x != 98 || y != 99)
 			{
 				putchar(',');
