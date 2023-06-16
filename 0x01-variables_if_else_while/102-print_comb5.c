4808ea9ac6d312aa4292a7b0f9c9b8b61440b651
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-16 23:56:20 +0200

    adding TASK:102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * function a program that prints all possible
+ * combinations of single-digit numbers.
+ * Return: 0 Always (SUCCESS)
+ *
+ */
+
+int main(void)
+{
+	int x, y;
+
+	while (x < 100)
+	{
+		while (y < 100)
+		{
+			putchar(x / 10 + '0');
+			putchar(x % 10 + '0');
+			putchar(' ');
+			putchar(y / 10 + '0');
+			putchar(y % 10 + '0');
+			if (x != 98 || y != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			y++;
+		}
+		x++;
+		y = x + 1;
+	}
+
+	putchar('\n');
+	return (0);
+}
