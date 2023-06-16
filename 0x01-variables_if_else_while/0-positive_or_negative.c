4f582b2bbc495a1e69851c08aa18b183b3c2a8da
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-16 09:06:02 -0700

    adding TASK:0-positive_or_negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* (MOODY) */
+
+/**
+ * main - Entry point
+ * function that gets a random value n, then prints information about it...
+ * Return: 0 SUCCESS
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive", n);
+	} else if (n < 0)
+	{
+		printf("%d is negative", n);
+	} else
+	{
+		printf("%d is zero", n);
+	}
+	
+	printf("\n");
+	return (0);
+}
