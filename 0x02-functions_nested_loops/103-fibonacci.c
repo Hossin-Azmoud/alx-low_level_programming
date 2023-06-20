06e9a7d52760832153ac00a43eed9a8ed374a439
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-21 00:43:48 +0200

    adding main.h

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints the first 50 fib numbers.
+ * Return: 0 Always (SUCCESS)
+ */
+
+int main(void)
+{
+	unsigned long m = 1;
+	unsigned long n = 2;
+	unsigned long sum = 0;
+	int i = 0;
+
+	while (i < 50)
+	{
+		if (m % 2 == 0)
+		{
+			sum += m;		
+		}
+
+		if (i < 49)
+		{
+			n += m;
+			m = n - m;
+		}
+
+		i++;
+	}
+
+	printf("%i\n", sum);
+	return (0);
+}
