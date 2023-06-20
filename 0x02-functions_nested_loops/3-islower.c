29d4421d6d58517f033b179d011b5c9241c793c1
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-20 17:12:45 +0200

    ...

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,10 +2,11 @@
 
 /**
  * _islower - checks if c is lowercase
+ * checks if c is lowercase or not
+ * @c: charachter to be checked
  * Return: 1 if c is lowercase and 0 otherwise
  */
-
 int _islower(int c)
 {
-	return (int) (c >= 'a' && c <= 'z');
+	return ((int) (c >= 'a' && c <= 'z'));
 }
