53a4fbd73277fdabb65a77e8899026ad6714c4e0
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-20 19:37:29 +0200

    ...

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,8 +19,9 @@ void times_table(void)
 			fflush(stdout);
 			num1 = first_number * (second_number + 1);
 			num2 = (first_number * (second_number + 1)) % 10;
-			condition1 = (int)(first_number * second_number == (first_number * second_number) % 10);
 			condition2 = (int)(num1 == num2);
+			num1 = first_number * second_number;	
+			condition1 = (int)(num1 == num1 % 10);
 			condition3 = (int)(second_number != 9);
 
 			if (condition1 && condition2 && condition3)
@@ -28,12 +29,13 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
+				continue;
 			}
+			
 			if (condition3)
 			{
 				_putchar(',');
 				_putchar(' ');
-				continue;
 			}
 		}
 
