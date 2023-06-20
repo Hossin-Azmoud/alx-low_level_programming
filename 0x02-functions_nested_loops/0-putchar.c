0ed1cc8df12fee6e141bc8fe6eeac82967bb6885
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-20 16:04:54 +0200

    ...

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,19 +4,17 @@
  * main - Entry point
  * function a program that prints _putchar
  * Return: 0 Always (SUCCESS)
- * 	
  */
-
 int main(void)
 {
 	char s[9] = "_putchar\n";
 	int  i = 0;
-	
-	while(i < 9)
+
+	while (i < 9)
 	{
 		_putchar(s[i]);
 		i++;
 	}
-	
+
 	return (0);
 }
