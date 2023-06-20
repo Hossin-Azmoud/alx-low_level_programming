e8f6bc3fe450b3c3ec55e5fb836cf79410674566
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-21 00:48:26 +0200

    adding main.h

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,13 +10,14 @@ int main(void)
 	unsigned long m = 1;
 	unsigned long n = 2;
 	unsigned long sum = 0;
+	unsigned long limit = 4000000;
 	int i = 0;
 
-	while (i < 50)
+	while (i < 50 && m < limit)
 	{
 		if (m % 2 == 0)
 		{
-			sum += m;		
+			sum += m;
 		}
 
 		if (i < 49)
