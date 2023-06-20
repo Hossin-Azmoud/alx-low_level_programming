fab93a3764d8bd55dd04abcac56b2c3dc97c614a
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-20 15:19:17 +0200

    adding TASK:0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * function a program that prints all possible
+ * combinations of single-digit numbers.
+ * Return: 0 Always (SUCCESS)
+ *
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
