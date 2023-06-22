ac357312f4341909fd4e13719032a0ba6d738954
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-22 16:39:57 +0200

    adding 5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,19 +15,23 @@ void more_numbers(void)
 	while (it < 10)
 	{
 		start = 0;
-		i = 0;
+
 		while (start < limit)
 		{
+		
 			if (start > 9)
 			{
-				buff[0] = ((start / 10) % 10) + '0';
+				
+				buff[0] = (start / 10) % 10 + '0';
 				buff[1] = (start % 10) + '0';
 				buff[2] = '\0';
-				
+				i = 0;
 				while (buff[i] != '\0')
 				{
 					_putchar(buff[i]);
+					i++;
 				}
+
 			} else
 			{
 				_putchar(start + '0');
