e87f9b5c8e242eb367c0b2e0669482c167725b62
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-23 00:13:17 +0200

    adding print_number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,24 +1,47 @@
 #include "main.h"
-/*
+#define INT_MIN__ "-2147483648"
+
+/**
 * print_number - prints a number n
+* takes a number and then prints each digit
 * @n: number to be printed.
 * Return: void
 */
 void print_number(int n)
 {
+	int i = 0;
+
+	if (n == -2147483648)
+	{
+		while (INT_MIN__[i])
+		{
+			_putchar(INT_MIN__[i]);
+			i++;
+		}
+
+		return;
+	}
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	
-	if (n == 0)
-	{
 
-		_putchar('0');
+	if (n <= 9 && n >= 0)
+	{
+		_putchar(n % 10 + '0');
 		return;
 	}
 
-	print_number(n / 10);
+	if (n == 0)
+	{
+		return;
+	}
+
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
 	_putchar(n % 10 + '0');
 }
