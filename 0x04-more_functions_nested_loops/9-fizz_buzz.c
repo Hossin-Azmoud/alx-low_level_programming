eed6562cccc2703975274b204c354002646bcc88
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-22 18:53:00 +0200

    adding fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,45 @@
+#include <string.h>
+#include <stdio.h>
+
+
+
+
+/**
+ * main - fizz buzz..
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int  it = 1;
+	char string[10];
+
+	while (it <= 100)
+	{
+		strcpy(string, "");
+
+		if (it % 3 == 0)
+		{
+			strcpy(string, "Fizz");
+		}
+
+		if (it % 5 == 0)
+		{
+			strcat(string, "Buzz");
+		}
+
+		if (strlen(string) > 0)
+		{
+			printf("%s", string);
+		} else 
+		{
+			printf("%i", it);
+		}
+	
+		printf(" ");
+		it++;
+	}
+	
+	printf("\n");
+	return (0);
+}
