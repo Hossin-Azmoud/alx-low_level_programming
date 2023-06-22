434606fd81dc264ee5fe146eabed74ae1337ff30
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-22 16:04:04 +0200

    adding 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_numbers - prints 0 -> 9 except 2 and 4..
+* Return: void
+*/
+void print_most_numbers(void)
+{
+	int start = 0;
+
+	while (start < 10)
+	{
+		if (start != 2 && start != 4)
+		{
+			_putchar(start + '0');
+		}
+
+		start++;
+	}
+
+	_putchar('\n');
+}
