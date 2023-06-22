31af7ed69429a3ca266f26808ff20c3a6192975f
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-22 15:42:49 +0200

    adding 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* mul - multiply two integers.
+* @a: number
+* @b: number
+* Return: a and b multiplied result.
+*/
+void print_numbers(void)
+{
+	int start = 0;
+
+	while (start < 10)
+	{
+		_putchar(start + '0');
+		start++;
+	}
+
+	_putchar('\n');
+}
