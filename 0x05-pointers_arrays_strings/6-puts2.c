8fe12c28fed1d02c005061c5e3bafa414c908d2b
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-26 20:40:50 +0200

    added 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _puts2 - a function that prints a string but steps one character.
+* @str: pointer to string to be printed.
+* Return: Void
+*/
+void puts2(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str += 2;
+	}
+
+	_putchar('\n');
+}
+
