4a2d84551ffe7b1fff7ea510149099d821a9b0a6
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-26 21:25:35 +0200

    adding 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strcpy - a function that coppies the content of src to dest.
+* @src: the source string
+* @dest: the destination string
+* Return: char* destination string.
+*/
+
+char *_strcpy(char *dest, char *src);
+{
+	int it = 0;
+
+	if (!dest || !src)
+		return (NULL);
+
+	while (src[it] != '\0')
+	{
+		dest[it] = src[it];
+		it++;
+	}
+
+	return (dest);
+}
