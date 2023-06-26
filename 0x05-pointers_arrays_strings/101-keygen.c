57443767aa7be5d76f5ee0b655e0b9230b28a28c
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-26 22:56:10 +0200

    adding 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <time.h>
+/* MOODY */
+/**
+ * main - program that generates random valid pwds
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int *password = malloc(100);
+	int it = 0, sum, k;
+
+	sum = 0;
+	srand(time(NULL));
+	while (it < 100)
+	{
+		password[it] = rand() % 78;
+
+		sum += (password[it] + '0');
+		putchar(password[it] + '0');
+
+		if ((2772 - sum) - '0' < 78)
+		{
+			k = 2772 - sum - '0';
+			sum += k;
+			putchar(k + '0');
+			break;
+		}
+
+		it++;
+	}
+
+	free(password);
+	return (0);
+}
