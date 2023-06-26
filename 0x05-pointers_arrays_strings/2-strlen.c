88c0479a030871f3d6253b6a2cea1be6ca563979
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-26 18:38:42 +0200

    adding 2-strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
-* swap_int - a function that gets the size of a string.
+* _strlen - a function that gets the size of a string.
 * @s: pointer to string to be processed.
 * Return: int size of the passed string.
 */
-
-
-int _strlen(char *s);
+int _strlen(char *s)
 {
 	int it = 0;
 
