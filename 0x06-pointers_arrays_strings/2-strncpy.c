0975ac0f9a9d0c814ed3f7d2b8041df234b5a16a
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-29 17:08:43 +0200

    adding 2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,7 +7,7 @@
 * @dest: destincation string
 * Return: char * result of copy operation
 */
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
 {
 	char *ptr = dest;
 
