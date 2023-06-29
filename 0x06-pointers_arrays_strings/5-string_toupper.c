d38de5b30b1a1ffe932a56d7851516ff154bbfa3
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-29 17:57:11 +0200

    adding 5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* string_toupper - function that capitalizes a string.
+* @s: string
+* Return: pointer to the resulting string
+*/
+char *string_toupper(char *s);
+{
+	char *ptr = s;
+
+	while (*ptr)
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+			*ptr -= 32;
+
+		ptr++;
+	}
+
+	return (s);
+}
