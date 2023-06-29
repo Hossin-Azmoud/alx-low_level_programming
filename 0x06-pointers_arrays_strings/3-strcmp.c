d7692ef1ad228827b8b65e070b463023fe4bd5bc
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-29 17:15:17 +0200

    adding 3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strcmp - function that compares two strings
+* @s1: string
+* @s2: string
+* Return: 0 if s1 == s2 and 1 otherwise
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int it = 0;
+
+	while (s1[it] && s2[it])
+	{
+		if (s1[it] != s2[it])
+			return (1);
+
+		it++;
+	}
+
+	if (s1[it] == '\0' && s2[it] == '\0')
+		return (1);
+
+	return (0);
+}
