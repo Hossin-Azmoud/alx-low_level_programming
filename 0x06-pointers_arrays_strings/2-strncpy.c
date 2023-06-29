e41217b63820d97f44eaec4a1e24cd1d05539666
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-06-29 17:07:13 +0200

    adding 2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _strncpy - function that copies n bytes from src to dest.
+* @n: amount of bytes to copy
+* @src: source string
+* @dest: destincation string
+* Return: char * result of copy operation
+*/
+char *_strncpy(char *dest, char *src, int n);
+{
+	char *ptr = dest;
+
+	while (*src && n > 0)
+	{
+		*ptr++ = *src++;
+		n--;
+	}
+
+	while (n > 0)
+	{
+		*ptr++ = '\0';
+		n--;
+	}
+	return (dest);
+}
+
+
