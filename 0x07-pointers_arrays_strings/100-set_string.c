e450b1aca58a8fd23728c729f4623a627a588596
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-07-03 19:38:47 +0200

    adding 100-set_sring.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/* MOODY */
+/**
+* print_diagsums - function that sets the value of a pointer to a char.
+* @s: double pointer to the string to be set
+* @to: pointer to string to be set to.
+* Return: void
+*/
+void set_string(char **s, char *to);
+{
+	*s = to;
+}
