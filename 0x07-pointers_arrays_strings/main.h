0a46e37576ea56407ec280f5d63edeec94397d22
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-07-03 19:27:28 +0200

    adding 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdio.h>
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
