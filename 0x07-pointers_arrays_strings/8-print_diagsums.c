fae3c5eeee51c6304913d7f48151fe8bda6fc6ad
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-07-03 19:26:15 +0200

    adding 8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/* MOODY */
+/**
+* print_chessboard - function that prints the sum of the two diagonals
+* of a square matrix of integers.
+* @a: Array
+* @size: size of the array
+* Return: void
+*/
+void print_diagsums(int *a, int size);
+{
+	int left = 0, right = 0, i;
+
+	for (i = 0; i < size; i++)
+	{
+		left  += a[i * size + i];
+		right += a[i * size + (size - i - 1)];
+	}
+
+	printf("%d, %d\n", left_sum, right_sum);
+}
