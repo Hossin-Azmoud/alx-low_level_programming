5d21ae45fc329cd3870c9ae35538a8b85ef59f8f
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-07-03 14:36:29 +0200

    adding 1-_memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _memset -  function that copies memory area.
+* @dest: memory address to copy to
+* @src: memory address to copy from
+* @n: the amount of slots that will be copied over
+* Return: result
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int it = 0;
+
+	while (it < n)
+	{
+		dest[it] = src[it];
+		it++;
+	}
+	
+	return (dest);
+}
