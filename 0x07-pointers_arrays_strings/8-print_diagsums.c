472f2bce6930c8de42e33289d5094c352eb0057e
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-07-03 19:30:39 +0200

    adding 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,14 +8,14 @@
 * @size: size of the array
 * Return: void
 */
-void print_diagsums(int *a, int size)
+void print_diagsums(int *a, int size);
 {
 	int left = 0, right = 0, i;
 
 	for (i = 0; i < size; i++)
 	{
-		left  += a[i * size + i];
-		right += a[i * size + (size - i - 1)];
+		left  += *(a + i * size + i);
+		right += *(a + i * size + (size - i - 1));
 	}
 
 	printf("%d, %d\n", left_sum, right_sum);
