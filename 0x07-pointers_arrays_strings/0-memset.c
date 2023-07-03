3843f54d0c9b53959956632b8af268d2d9c279a9
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-07-03 14:28:41 +0200

    adding 0x07 project

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _memset -  function that fills memory with a constant byte.
+* @s: memory address that will be filled
+* @b: constant that will be used to fill the memory.
+* @n: the amount of slots that will be filled
+* Return: Result of filling.
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int it = 0;
+	
+	while(it < n)
+	{
+		s[it] = b;
+		it++;
+	}
+
+	return s;
+}
