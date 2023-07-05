d75c486e28744cf424cde06c008de1d7985776ae
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-07-05 18:37:48 +0200

    adding palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* is_palindrome - return if s is a palindrome
+* @s: string to be processed
+* Return: int 1 if is a palindrome and 0 otherwiser
+*/
+
+int is_palindrome(char *s)
+{
+	return (check_palindrome(s, 0, _strlen_recursion(s) - 1));
+}
+
+/**
+* check_palindrome - return if s is a palindrome
+* @s: string to be processed
+* @left: left pointing index
+* @right: right pointing index
+* Return: int 1 if is a palindrome and 0 otherwiser
+*/
+int check_palindrome(char *s, int left, int right)
+{
+	if (left >= right)
+		return (1);
+
+	if (s[left] != s[right])
+		return (0);
+
+	return (check_palindrome(s, left + 1, right - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,8 +8,9 @@ int  _strlen_recursion(char *s);
 int  factorial(int n);
 int  _pow_recursion(int x, int y);
 int  _sqrt_recursion(int n);
-int _find_sqrt(int n, int i);
-int is_prime_number(int n);
-int _is_p(int n, int i);
+int  _find_sqrt(int n, int i);
+int  is_prime_number(int n);
+int  _is_p(int n, int i);
+int  is_palindrome(char *s);
 
 #endif /* MAIN_H */
