8f91cfacf0bb5eeb21104b0558baa31059dd73b3
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-07-07 15:03:54 +0200

    adding 0x0A-argc_argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - program that prints the number of passed args.
+* @argv: array of argumenets
+* @argc: length of argv
+* Return: 0 SUCCESS
+*/
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%i\n", argc);
+	return (0);
+}
