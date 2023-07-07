43cb330a9f81dbe47fc2d954de15c0b1b4317123
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-07-07 15:23:27 +0200

    adding 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
 * main - program that multiplies two numbers
 * @argv: array of argumenets
@@ -8,16 +8,18 @@
 */
 int main(int argc, char *argv[])
 {
+	int a;
+	int b;
+
 	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int a = atoi(argv[1]);
-	int b = atoi(argv[2]);
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
 
 	printf("%i\n", (a * b));
-
 	return (0);
 }
