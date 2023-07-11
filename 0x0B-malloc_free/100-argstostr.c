ccb4a95c9f955609813600a10577869b94c3124a
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-07-11 15:08:17 +0200

    adding 100-args

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+* _strlen - function that calcs the length of a string.
+* @s: string that will be concatenated
+* Return: (int) length of s
+*/
+int _strlen(char *s)
+{
+	int ln = 0;
+
+	if (s == NULL)
+		return (ln);
+
+	while (s[ln] != '\0')
+		ln++;
+
+	return (ln);
+}
+
+/**
+* argstostr - a function that concats all args of the program.
+* @ac: arg count
+* @av: arg vector
+* Return: String that concats av.
+*/
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int ln = 0;
+	int i, j, it = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+		ln += _strlen(av[i]);
+
+	str = malloc(sizeof(char) * (ln + 1));
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, it++)
+		{
+			str[it] = av[i][j];
+		}
+	}
+
+	str[ln] = '\0';
+	return (str);
+}
+
