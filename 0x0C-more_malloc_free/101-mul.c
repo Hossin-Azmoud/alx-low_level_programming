849edb62498a79050dc0d2dc54328cdf757fae5f
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-07-14 00:27:06 +0200

    adding mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 /**
  * is_digits - a function that checks if a buffer has only digits.
  * @buffer: buffer that will be tested.
@@ -18,7 +19,7 @@ int is_digits(char *buffer)
 		iter++;
 	}
 
-	return (1);
+	return iter;
 }
 
 /**
@@ -32,7 +33,7 @@ int main(int argc, char *argv[])
 
 	char *buff1;
 	char *buff2;
-
+	int count_1, count_2;
 	if (argc < 3)
 	{
 		printf("Error\n");
@@ -41,12 +42,17 @@ int main(int argc, char *argv[])
 
 	buff1 = argv[1];
 	buff2 = argv[2];
+	count_1 = is_digits(buff1);
+	count_2 = is_digits(buff2);
 
-	if ((is_digits(buff1) == 0) || (is_digits(buff2) == 0))
+	if ((count_1 == 0) || (count_2 == 0))
 	{
 		printf("Error\n");
 		return (98);
 	}
+
+	if (count_1 > 19 || count_2 > 19)
+		return (0);
 
 	printf("%i\n", atoi(buff1) * atoi(buff2));
 	return (0);
