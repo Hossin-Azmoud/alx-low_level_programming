7c90d422756cb0e5952bc4dc1f536b2bba127796
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-07-17 22:09:52 +0200

    adding 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - entry point.
+* a program that prints the name of tha file it was compiled in.
+* Return: 0 Always SUCCESS
+*/
+int main(void)
+{
+	printf("%s\n", __file__);
+	return 0;
+}
