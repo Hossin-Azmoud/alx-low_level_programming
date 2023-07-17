f1abccaf869beff2bafde7f59a4b33095af880c4
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-07-17 22:34:54 +0200

    Daaaaaaaaaaawg

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog_t - a struct that represents a dog.
+ * @name: the name of the dawg.
+ * @age: the age of the dawg.
+ * @owner: the name of the owner of the dawg.
+ */
+struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
