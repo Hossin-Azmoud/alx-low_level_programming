beb0ce6e9060ec9af5c562aac82ba05912046107
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-07-19 15:50:08 +0200

    adding func pointers.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+* print_name - function that prints the name
+* @name: name to be printed.
+* @f:    pointer to function that will handle printing.
+* Return: Void (ALWAYS)
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	return f(name);
+}
