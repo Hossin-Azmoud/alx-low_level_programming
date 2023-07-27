e71e9d3041e9c69dc256d3d9194f11a733b23282
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-07-27 14:19:36 +0100

    betty fix.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,7 +11,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (new_node == NULL)
 		return (NULL);
-	
+
 	new_node->str  = strdup(str);
 	new_node->len  = strlen(str);
 	new_node->next = *head;
