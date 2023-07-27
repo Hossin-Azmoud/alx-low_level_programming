9dd521794a6080ed4330f4d6a68fb752c9e131e0
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-07-27 20:39:26 +0100

    added _strlen

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,5 +1,23 @@
 #include "lists.h"
 /**
+ * _strlen - function to check length of a string.
+ * @s: String.
+ * Return: len of s
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int idx = 0;
+
+	if (s == NULL)
+		return (idx);
+
+	while (s[idx]!= 0)
+		idx++;
+
+	return (idx);
+}
+
+/**
 * add_node_end - function that adds a node to the end of the list.
 * @head: refrence to the start of the list.
 * @str: data that will be held by the node.
@@ -14,7 +32,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (new);
 
 	new->str = strdup(str);
-	new->len = strlen(str);
+	new->len = _strlen(str);
 
 	if (*head == NULL)
 	{
