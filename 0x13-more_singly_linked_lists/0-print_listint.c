0235146a34e6136957a35753ccda8921317aba16
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-07-31 21:51:12 +0200

    .

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+* print_listint - prints all elements from a list.
+* @h: pointer to the list.
+* Return: Size of the list.
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t size = 0;
+
+	printf("%i\n", h->n);
+	if (h->next != NULL)
+		size += print_listint(h->next);
+
+	return (size);
+}
