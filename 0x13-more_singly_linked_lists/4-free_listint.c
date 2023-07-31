6ba54cd4a08ab04479a4d57e1dc2e832f719ab67
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-07-31 22:36:33 +0200

    adding free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+/**
+* free_listint - a function that frees the list.
+* @head: a pointer to the head of the list that will be freed.
+* Return: always void
+*/
+void free_listint(listint_t *head)
+{
+	listint_t current = head;
+
+	while (current != NULL)
+	{
+		free(current);
+		current = current->next;
+	}
+}
