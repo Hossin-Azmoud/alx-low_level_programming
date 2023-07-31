8700c154824d958617113be2b501513d383196fd
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-07-31 23:24:37 +0200

    .

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+* free_listint2 - a func that frees a llist then sets the head to null
+* @head: head of the list.
+* Return: always void
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *tmp;
+
+	if (*head == NULL)
+		return;
+
+	while (current != NULL)
+	{
+		tmp = current->next;
+		free(current);
+		current = tmp;
+	}
+
+	*head = NULL;
+}
