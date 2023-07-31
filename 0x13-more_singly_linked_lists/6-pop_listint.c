5719b883d5b017d5bd63888de1ae9af5cb2cf463
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-07-31 23:37:14 +0200

    .

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,9 +1,7 @@
 #include "lists.h"
 /**
  * pop_listint - deletes the head, and returns the heads node data
- *
  * @head: ** head of the linked list.
- *
  * Return: head data or 0 otherwise if failure
  */
 int pop_listint(listint_t **head)
@@ -20,5 +18,5 @@ int pop_listint(listint_t **head)
 
 	free(new_head);
 
-	return (n);
+	return (num);
 }
