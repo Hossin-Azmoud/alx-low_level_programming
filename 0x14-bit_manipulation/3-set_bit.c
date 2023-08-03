3e3920cb23af8570a00b130daf4d0b49bdad7d42
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-08-03 20:09:33 +0200

    adding 3-set_bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * set_bit - sets a bit.
+ * @n: number to proc.
+ * @index: index of the bit to set.
+ * Return: The result.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > (sizeof(*n) * 8 - 1))
+		return (-1);
+
+	*n |= 1 << index;
+	return (0);
+}
