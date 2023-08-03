5967472a40f4fddbc9f459a7a80a9944b00362f5
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-08-03 19:51:51 +0200

    adding 2-get_bit

--- 0x14-bit_manipulation/main.c
+++ 0x14-bit_manipulation/main.c
@@ -3,7 +3,6 @@
 
 /**
  * main - check the code
- *
  * Return: Always 0.
  */
 int main(void)
@@ -16,5 +15,8 @@ int main(void)
     printf("%d\n", n);
     n = get_bit(1024, 0);
     printf("%d\n", n);
-    return (0);
+    n = get_bit(UINT_MAX, 0);
+    printf("%d\n", n);
+
+	return (0);
 }
