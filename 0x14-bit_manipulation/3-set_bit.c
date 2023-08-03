cce3ccfda4d8fd6a8c5d4f107e7f4e0cce264ffc
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-08-03 20:14:31 +0200

    adding 3-set_bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,9 +7,10 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	/* NOTE: (N |= 1 << K) */
 	if (index > (sizeof(*n) * 8 - 1))
 		return (-1);
 
-	*n |= (1 << index);
+	*n = (*n | (1 << index));
 	return (0);
 }
