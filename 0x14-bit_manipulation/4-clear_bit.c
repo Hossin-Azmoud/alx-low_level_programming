1b937ec2debd88b103ad4afdf13207216716184f
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-08-03 20:22:07 +0200

    adding 3-set_bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,10 +7,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	/* NOTE: (N |= 0 << K) */
+	/* NOTE: n = n & ~(1 << k)*/
 	if (index > (sizeof(*n) * 8 - 1))
 		return (-1);
 
-	*n = (*n | (0 << index));
+	*n = (*n & ~(1 << index));
 	return (1);
 }
