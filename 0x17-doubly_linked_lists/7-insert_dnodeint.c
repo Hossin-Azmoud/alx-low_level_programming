433ab3f0426505628d13720ad3ab36c7bd688761
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-09-14 17:05:40 +0200

    adding (7) insert node into somewhere (index)

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -30,7 +30,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new->prev       = at_index->prev;
 		new->prev->next = new;
 		at_index->prev  = new;
-
 		return (new);
 	}
 
