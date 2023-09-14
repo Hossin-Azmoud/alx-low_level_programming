c22f4b9a91ae08eab6393692f03b6ed7bf732987
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-09-14 16:25:13 +0200

    adding (5) get index at (i) using recursion

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,7 +1,11 @@
 #include "lists.h"
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	if (!head)
+		return (NULL);
+
 	if (index == 0)
 		return (head);
+
 	return get_dnodeint_at_index(head->next, (index - 1));
 }
