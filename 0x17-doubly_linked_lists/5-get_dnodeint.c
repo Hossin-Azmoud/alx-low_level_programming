9fda0d267248e0dbada29cacb08d9b5c26ad3a77
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-09-14 16:27:52 +0200

    adding (5) get index at (i) using recursion

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,4 +1,11 @@
 #include "lists.h"
+/**
+* get_dnodeint_at_index - func that gets the node at index (index)
+* @head: head of the list.
+* @index: index we want.
+* Return: node at index (index)
+*/
+
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	if (!head)
@@ -7,5 +14,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	if (index == 0)
 		return (head);
 
-	return get_dnodeint_at_index(head->next, (index - 1));
+	return (get_dnodeint_at_index(head->next, (index - 1)));
 }
