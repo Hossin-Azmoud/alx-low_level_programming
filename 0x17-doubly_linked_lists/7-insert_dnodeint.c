e3c5c9429a5ab9090615bc4999fba7f3e5987be3
Hossin-Azmoud <AzmoudHossin0101@gmail.com>
2023-09-14 16:51:56 +0200

    adding (7) insert node into somewhere (index)

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *at_index = *h;
 	dlistint_t  *new      = malloc(sizeof(dlistint_t));
 
-	if (!h || !n)
+	if (!h || !idx)
 		return (add_dnodeint(h, n));
 
 	if (!new)
